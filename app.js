da351deb3d6b2a3a8113f62283e48be3e749502d
paulrivera1994 <100796671+paulrivera1994@users.noreply.github.com>
2023-01-03 13:22:13 -0700

    Finished all exercises

--- app.js
+++ app.js
@@ -7,13 +7,45 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
-
+let a = 20;
+let b = 4;
+let add = a + b; // 24
+let minus = a - b; // 16
+let multiply = a * b; // 80
+let divide = a / b; // 5
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
-
 // YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+// Answers to questions (in order)
+// 1111
+// '11eleven'
+// 12
+// 'frodo11'
+// 'true11'
+// 'FrodoBaggins'
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+console.log("eleven" == str4 && val >= str3);
+// Answers to questions (in order)
+// true
+// false
+// true
+// false
+// true
+// true
+// false
+// false
+// true
